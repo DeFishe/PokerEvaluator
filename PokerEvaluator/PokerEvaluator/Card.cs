12261f0f4456a6ab0e06558ce9bfa9791f5cb120
Darian Fisher <43389055+DeFishe@users.noreply.github.com>
2020-02-24 13:46:04 -0500

    Inital commit
    Started work on Card and Deck classes.

---
+++ PokerEvaluator/PokerEvaluator/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerEvaluator
+{
+    class Card
+    {
+        string Suit { get; set; }
+        string Value { get; set; }
+        private static List<string> PossibleSuits = new List<string>()
+        {
+            "Club",
+            "Spade",
+            "Heart",
+            "Diamond"
+        };
+        private static List<string> PossibleValues = new List<string>()
+        {
+            "1","2","3","4","5","6","7","8","10","Jack","Queen","King"
+        };
+
+        public Card(int deckPosition)
+        {
+            int suitSelection = deckPosition % 4;
+            int valueSelection = deckPosition % 13;
+            Suit = PossibleSuits.ElementAt(suitSelection);
+            Value = PossibleValues.ElementAt(valueSelection);
+        }
+    }
+}
---
+++ PokerEvaluator/PokerEvaluator/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerEvaluator
+{
+    class Deck
+    {
+        List<Card> cards = new List<Card>();
+
+        public Deck()
+        {
+            for (int deckPosition = 0; deckPosition < 52; deckPosition++)
+            {
+                cards.Add(new Card(deckPosition));
+            }
+        }
+    }
+}
